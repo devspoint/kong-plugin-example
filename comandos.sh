f4cc74beb873535fd90b6ca5ecb15aaee7ba6479
Marcelo Marinho <marcelo.marinho@luizalabs.com>
2021-12-06 08:37:57 -0300

    gifs execução

--- comandos.sh
+++ comandos.sh
@@ -1,2 +1,21 @@
 echo ""
 
+echo "\n\n#############################################################"
+echo "\nExecutar rota mensagem_um"
+curl --location --request GET 'http://localhost:8000/mensagem_um'
+sleep 1
+
+echo "\n\n#############################################################"
+echo "\nExecutar rota mensagem_dois"
+curl --location --request GET 'http://localhost:8000/mensagem_dois'
+sleep 1
+
+echo "\n\n#############################################################"
+echo "\nExecutar rota mensagem_tres"
+curl --location --request GET 'http://localhost:8000/mensagem_tres'
+sleep 1
+
+echo "\n\n#############################################################"
+echo "\nExecutar rota mensagem_quatro"
+curl --location --request GET 'http://localhost:8000/mensagem_quatro'
+sleep 1
--- kong/plugins/kong-example/handler.lua
+++ kong/plugins/kong-example/handler.lua
@@ -8,12 +8,38 @@ function KongExample:new()
   KongExample.super.new(self, "kong-example")
 end
 
+function KongExample:init_worker(config)
+  print('KongExample:init_worker\n\n\n\n')
+  print('\n\n')
+end
+
+function KongExample:rewrite(config)
+  print('KongExample:rewrite\n\n\n\n')
+  print('\n\n')
+end
+
+function KongExample:header_filter(config)
+  print('KongExample:header_filter')
+  print('\n\n')
+end
 
 function KongExample:access(config)
   print('KongExample:access')
   print('Name ', config.name)
   print('Number ', config.number)
+  print('\n\n')
 end
 
+function KongExample:body_filter(config)
+  print('KongExample:body_filter')
+  local chunk, eof = ngx.arg[1], ngx.arg[2]
+  print('chunk=',chunk)
+  print('eof=',eof)
+  print('\n\n\n')
 
+end
+function KongExample:log(config)
+  print('KongExample:log')
+  print('\n\n')
+end
 return KongExample
